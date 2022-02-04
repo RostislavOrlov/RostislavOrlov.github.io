dcce9a583b27284ddf755c775c50f8514dbbf069
RostislavOrlov <90448930+RostislavOrlov@users.noreply.github.com>
2022-02-04 22:30:22 +0300

    Add files via upload

---
+++ slider3.js
@@ -0,0 +1,71 @@
+"use strict"
+var $popOverlay = $(".popup-overlay");
+var $popWindow = $(".popWindow");
+var $popupMainWindow = $(".popup-main-window");
+var $popThankYouWindow = $(".thank-you-window");
+var $popClose = $(".close-btn");
+var $popOpen = $("#for-popup-form");
+
+$(function() {
+// Close Pop-Up after clicking on the button "Close"
+$popClose.on("click", function() {
+history.replaceState(null, null, ' ');
+$popOverlay.fadeOut();
+$popWindow.fadeOut();
+$popThankYouWindow.fadeOut();
+});
+
+$(document).on("click", function(event) {
+if ($(event.target).closest($popWindow).length) return;
+history.replaceState(null, null, ' ');
+$popOverlay.fadeOut();
+$popWindow.fadeOut();
+$popThankYouWindow.fadeOut();
+event.stopPropagation();
+});
+
+
+$(".popup-form").submit(function() {
+    var th = $(this);
+    $(".popup-submit").prop('disabled', true);
+    $.ajax({
+    type: "POST",
+    url: "https://formcarry.com/s/eayAMlodaPh",
+    data: th.serialize(),
+    })
+    //.done(function() {
+    // после успешной отправки скрываем форму подписки и выводим окно с благодарностью за заполнение формы
+    $popupMainWindow.fadeOut();
+    $popThankYouWindow.fadeIn();
+    // очищаем форму
+    setTimeout(function() {
+    $(".popup-submit").prop('disabled', false);
+    th.trigger("reset");
+    }, 1000);
+    //});
+    return false;
+    });
+});
+
+$(document).ready(function() {
+$popOpen.click(function(){
+window.location.hash = "#popup";
+$popOverlay.fadeIn();
+$popupMainWindow.fadeIn();
+return false;
+});
+});
+
+$(window).on('hashchange', function (event) { //при клике на "назад" скрывать форму
+if(window.location.hash == "#popup"){
+$popOverlay.fadeIn();
+$popupMainWindow.fadeIn();
+}
+else{
+if(window.location.hash != "#popup") {
+$popOverlay.fadeOut();
+$popupMainWindow.fadeOut();
+$popThankYouWindow.fadeOut();
+}
+}
+});
